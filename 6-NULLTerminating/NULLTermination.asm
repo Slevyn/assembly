d765fed8a7998832f3a13889a8927de9838020be
slevyn <davidjblackford@gmail.com>
2016-11-16 13:17:51 -0500

    Added 6-NullTerminating... but it doesn't work quite right.

--- 6-NULLTerminating/NULLTermination.asm
+++ 6-NULLTerminating/NULLTermination.asm
@@ -1,17 +1,17 @@
 %include 'functions.asm'
 
+section .data
+	msg1 db "Hello, World", 0xa, 0x00
+	msg2 db "Goodbye", 0xa, 0x00
+
 section .text
 global _start
 
 _start:
 	MOV EAX, msg1
 	CALL sprint
-	
+
 	MOV EAX, msg2
 	CALL sprint
 
 	CALL quit
-
-section .data
-	msg1 db "Hello, brave new world!", 0Ah
-	msg2 db "This is how we recycle in NASM.", 0Ah
